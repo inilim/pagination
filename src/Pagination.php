a1338f946bf4b47e56e23da130d0df4ba5cb0cde
Dmitry <40324321+inilim@users.noreply.github.com>
2023-12-25 23:10:52 +0300

    ref

--- src/Pagination.php
+++ src/Pagination.php
@@ -7,12 +7,16 @@ use Inilim\Pagination\ResultPagination;
 class Pagination
 {
     /**
-     * @param integer $cur_page текущая страница
-     * @param integer $limit количество записей на одну страницу
-     * @param integer $count_record общее количество записей
+     * @param int $cur_page текущая страница
+     * @param int $limit количество записей на одну страницу
+     * @param int $count_record общее количество записей
      */
     public function getAll(int $cur_page, int $limit, int $count_record): ResultPagination
     {
+        $cur_page     = $this->prepareCurPage($cur_page);
+        $limit        = abs($limit);
+        $count_record = abs($count_record);
+
         return new ResultPagination(
             count_page: $count_page = $this->getCountPage($limit, $count_record),
             count_record: $count_record,
@@ -26,67 +30,110 @@ class Pagination
         );
     }
 
+    /**
+     * @param int $cur_page текущая страница
+     * @param int $count_page общее количество страниц Применяется abs()
+     */
     public function nextPage(int $cur_page, int $count_page): ?int
     {
-        $cur_page = $this->getValidCurPage($cur_page, $count_page);
+        $count_page = $this->prepareCountPage($count_page);
+        $cur_page   = $this->getValidCurPage($cur_page, $count_page);
         if ($cur_page === $count_page || $count_page === 1) return null;
         return $cur_page + 1;
     }
 
+    /**
+     * @param int $cur_page текущая страница
+     * @param int $count_page общее количество страниц Применяется abs()
+     */
     public function prevPage(int $cur_page, int $count_page): ?int
     {
-        $cur_page = $this->getValidCurPage($cur_page, $count_page);
+        $count_page = $this->prepareCountPage($count_page);
+        $cur_page   = $this->getValidCurPage($cur_page, $count_page);
         if ($cur_page === 1) return null;
         return $cur_page - 1;
     }
 
     /**
      * Получить общее количество страниц
-     * @param integer $limit количество записей на одну страницу
-     * @param integer $count_record общее количество записей
-     * @return integer
+     * @param int $limit количество записей на одну страницу Применяется abs()
+     * @param int $count_record общее количество записей Применяется abs()
+     * @return int<1,max>
      */
     public function getCountPage(int $limit, int $count_record): int
     {
-        $r = intval(ceil(intval($count_record) / $limit));
-        return $r === 0 ? 1 : $r;
+        $limit        = abs($limit);
+        $count_record = abs($count_record);
+        $c = intval(ceil(intval($count_record) / $limit));
+        return $c === 0 ? 1 : $c;
     }
 
     /**
-     * расчитываем offset
-     * @param integer $cur_page текущая страница
-     * @param integer $count_page общее количество страниц
-     * @param integer $limit количество записей на одну страницу
-     * @return integer offset
+     * расчитываем offset для sql запроса
+     * @param int $cur_page текущая страница
+     * @param int $count_page общее количество страниц Применяется abs()
+     * @param int $limit количество записей на одну страницу Применяется abs()
+     * @return int<0,max>
      */
     public function getOffset(int $cur_page, int $count_page, int $limit): int
     {
-        $cur_page = $cur_page <= 0 ? 1 : $cur_page;
-        $offset = $cur_page > $count_page ? $count_page : $cur_page;
-        $offset = ($offset * $limit) - $limit;
-        return ($offset < 0) ? 0 : $offset;
+        $cur_page = $this->getValidCurPage($cur_page, $count_page);
+        $limit  = abs($limit);
+        $offset = ($cur_page * $limit) - $limit;
+        return $offset < 0 ? 0 : $offset;
     }
 
     /**
-     * @param integer $cur_page текущая страница
-     * @param integer $count_page общее количество страниц
+     * @param int $cur_page текущая страница
+     * @param int $count_page общее количество страниц Применяется abs()
+     * @return int<1,max>
      */
     public function getValidCurPage(int $cur_page, int $count_page): int
     {
-        $cur_page = $cur_page <= 0 ? 1 : $cur_page;
+        $cur_page   = $this->prepareCurPage($cur_page);
         if ($cur_page === 1) return 1;
-        $cur_page = ($cur_page > $count_page) ? $count_page : $cur_page;
+        $count_page = $this->prepareCountPage($count_page);
         return $cur_page > $count_page ? $count_page : $cur_page;
     }
 
+    /**
+     * @param int $cur_page текущая страница
+     * @param int $count_page общее количество страниц Применяется abs()
+     */
     public function isLastPage(int $cur_page, int $count_page): bool
     {
-        $cur_page = $cur_page <= 0 ? 1 : $cur_page;
+        $count_page = $this->prepareCountPage($count_page);
+        $cur_page   = $this->prepareCurPage($cur_page, $count_page);
         return $cur_page >= $count_page;
     }
 
+    /**
+     * @param int $cur_page текущая страница
+     */
     public function isFirstPage(int $cur_page): bool
     {
-        return $cur_page <= 1;
+        $cur_page = $this->prepareCurPage($cur_page);
+        return $cur_page === 1;
+    }
+
+    // ------------------------------------------------------------------
+    // protected
+    // ------------------------------------------------------------------
+
+    /**
+     * @return int<1,max>
+     */
+    protected function prepareCurPage(int $cur_page): int
+    {
+        return $cur_page <= 0 ? 1 : $cur_page;
+    }
+
+    /**
+     * @return int<1,max>
+     */
+    protected function prepareCountPage(int $count_page): int
+    {
+        $count_page = abs($count_page);
+        return $count_page === 0 ? 1 : $count_page;
     }
 }
