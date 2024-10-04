900c80fea9b24d3d4b2687b0db02617e943bde45
Dmitry <inipel@ya.ru>
2024-10-04 16:19:23 +0300

    ref

--- Tests/test.php
+++ Tests/test.php
@@ -1,3 +1,13 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+use Inilim\Pagination\Pagination;
+use Inilim\Dump\Dump;
+
+Dump::init();
+
+$pag = new Pagination;
+
+$res = $pag->getAll(1, 10, 1);
+de($res);
